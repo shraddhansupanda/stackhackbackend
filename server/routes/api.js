281c10810748e1918fb11e50cdd40614f482fd2a
shraddhansupanda <pandataj6@gmail.com>
2020-06-04 20:22:41 +0530

    nav bar build

--- server/routes/api.js
+++ server/routes/api.js
@@ -18,6 +18,93 @@ mongoose.connect(db,err=>{
 router.get('/',function(req,res){
     res.send('hello from api')
 })
+
+router.get('/events', (req,res) => {
+    let events = [
+      {
+        "_id": "1",
+        "name": "Auto Expo",
+        "description": "lorem ipsum",
+        "date": "2012-04-23T18:25:43.511Z"
+      },
+      {
+        "_id": "2",
+        "name": "Auto Expo",
+        "description": "lorem ipsum",
+        "date": "2012-04-23T18:25:43.511Z"
+      },
+      {
+        "_id": "3",
+        "name": "Auto Expo",
+        "description": "lorem ipsum",
+        "date": "2012-04-23T18:25:43.511Z"
+      },
+      {
+        "_id": "4",
+        "name": "Auto Expo",
+        "description": "lorem ipsum",
+        "date": "2012-04-23T18:25:43.511Z"
+      },
+      {
+        "_id": "5",
+        "name": "Auto Expo",
+        "description": "lorem ipsum",
+        "date": "2012-04-23T18:25:43.511Z"
+      },
+      {
+        "_id": "6",
+        "name": "Auto Expo",
+        "description": "lorem ipsum",
+        "date": "2012-04-23T18:25:43.511Z"
+      }
+    ]
+    res.json(events)
+  })
+  
+  router.get('/special', (req, res) => {
+    let specialEvents = [
+      {
+        "_id": "1",
+        "name": "Auto Expo Special",
+        "description": "lorem ipsum",
+        "date": "2012-04-23T18:25:43.511Z"
+      },
+      {
+        "_id": "2",
+        "name": "Auto Expo Special",
+        "description": "lorem ipsum",
+        "date": "2012-04-23T18:25:43.511Z"
+      },
+      {
+        "_id": "3",
+        "name": "Auto Expo Special",
+        "description": "lorem ipsum",
+        "date": "2012-04-23T18:25:43.511Z"
+      },
+      {
+        "_id": "4",
+        "name": "Auto Expo Special",
+        "description": "lorem ipsum",
+        "date": "2012-04-23T18:25:43.511Z"
+      },
+      {
+        "_id": "5",
+        "name": "Auto Expo Special",
+        "description": "lorem ipsum",
+        "date": "2012-04-23T18:25:43.511Z"
+      },
+      {
+        "_id": "6",
+        "name": "Auto Expo Special",
+        "description": "lorem ipsum",
+        "date": "2012-04-23T18:25:43.511Z"
+      }
+    ]
+    res.json(specialEvents)
+  })
+
+
+
 router.post('/register',(req,res)=>{
     let userData=req.body
     let user=new User(userData)
@@ -30,5 +117,24 @@ router.post('/register',(req,res)=>{
         }
     })
 })
-        
+router.post('/post',(req,res)=>{
+    let userData=req.body
+
+    User.findOne({email:userData.email},(error,user)=>{
+        if (error){
+            console.log(error)
+        }else{
+            if(!user){
+            res.status(401).send('Invalid email')
+            }else{
+                if (user.password != userData.password){
+                    res.status(401).send('Ivalid password')
+                }else{
+                    res.status(200).send(user)
+                }
+            }
+        }
+    })
+
+})       
 module.exports=router
